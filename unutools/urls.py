9479be103613fa2d49ea4a3ea464c3c53e8935bb
Kislov Dmitry <kislov@easydan.com>
2017-02-02 16:53:25 +1000

    Equipment->Unutool renaming

--- unutools/urls.py
+++ unutools/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import *
-from bgi.equipment.views import (request_rent, equipment_list,
+from bgi.unutools.views import (request_rent, equipment_list,
                             delete_rent_app)
 
 urlpatterns = patterns('',
-   url(r'^$', request_rent, name="equipment-app"),
-   url(r'^show$', equipment_list, name="equipment-list"),
+   url(r'^$', request_rent, name="unutool-app"),
+   url(r'^show$', equipment_list, name="unutool-list"),
    url(r'^del$', delete_rent_app, name="delete_rent_app")
-   
+
 )
